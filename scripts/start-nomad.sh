5ea521027fe7ae8c96fdbde18945a1ef9e52c6a4
Michele <mdeggies@gmail.com>
2021-05-26 15:55:42 -0700

    Troubleshoot

--- scripts/start-nomad.sh
+++ scripts/start-nomad.sh
@@ -6,7 +6,7 @@ export VAULT_ADDR=http://localhost:8200
 if [ ! -e /tmp/vault-test.pid ]; then
     vault server -dev -dev-root-token-id=$VAULT_TEST_TOKEN > /dev/null 2>&1 &
 
-    export VAULT_PID=$!
+    VAULT_PID=$!
     echo $VAULT_PID > /tmp/vault-test.pid
 else
     echo "Vault server already running"
@@ -15,7 +15,7 @@ fi
 if [ ! -e /tmp/consul-test.pid ]; then
     consul agent -dev > /dev/null 2>&1 &
 
-    export CONSUL_PID=$!
+    CONSUL_PID=$!
     echo $CONSUL_PID > /tmp/consul-test.pid
 else
     echo "Consul agent already running"
@@ -23,8 +23,8 @@ fi
 
 if [ ! -e /tmp/nomad-test.pid ]; then
     echo "Nomad agent not running, starting it.."
-    sudo nomad agent -dev -bind 0.0.0.0 -acl-enabled -vault-address=$VAULT_ADDR -vault-token $VAULT_TEST_TOKEN -vault-enabled -vault-allow-unauthenticated=false > /dev/null 2>&1 &
-    export NOMAD_PID=$!
+    sudo nomad agent -dev -bind 0.0.0.0 -acl-enabled -vault-address=$VAULT_ADDR -vault-token $VAULT_TEST_TOKEN -vault-enabled -vault-allow-unauthenticated=false
+    NOMAD_PID=$!
     echo $NOMAD_PID > /tmp/nomad-test.pid
 
     # Give some time for the process to initialize
@@ -33,9 +33,9 @@ if [ ! -e /tmp/nomad-test.pid ]; then
     nomad node status
     http --ignore-stdin POST http://localhost:4646/v1/acl/bootstrap | jq -r '.SecretID' > /tmp/nomad-test.token
     echo export NOMAD_TOKEN=$(cat /tmp/nomad-test.token)
-    export $NOMAD_TOKEN >> $GITHUB_ENV
+    echo export $NOMAD_TOKEN >> $GITHUB_ENV
 elif [ -e /tmp/nomad-test.token ]; then 
   echo "Nomad agent already running"
   echo export NOMAD_TOKEN=$(cat /tmp/nomad-test.token)
-  export $NOMAD_TOKEN >> $GITHUB_ENV
+  echo export $NOMAD_TOKEN >> $GITHUB_ENV
 fi
