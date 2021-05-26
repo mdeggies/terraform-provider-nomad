af8b7ede32e2197305a090258d00e5294e93dcd8
Michele <mdeggies@gmail.com>
2021-05-26 13:25:36 -0700

    Troubleshooting

--- scripts/start-nomad.sh
+++ scripts/start-nomad.sh
@@ -9,6 +9,9 @@ if [ ! -e /tmp/vault-test.pid ]; then
     export VAULT_PID=$!
     echo $VAULT_PID > /tmp/vault-test.pid
 fi
+else
+    echo "Vault server already running"
+fi
 
 if [ ! -e /tmp/consul-test.pid ]; then
     consul agent -dev > /dev/null 2>&1 &
@@ -16,9 +19,13 @@ if [ ! -e /tmp/consul-test.pid ]; then
     export CONSUL_PID=$!
     echo $CONSUL_PID > /tmp/consul-test.pid
 fi
+else
+    echo "Consul agent already running"
+fi
 
 if [ ! -e /tmp/nomad-test.pid ]; then
-    nomad agent -dev -bind 0.0.0.0 -acl-enabled -vault-address=$VAULT_ADDR -vault-token $VAULT_TEST_TOKEN -vault-enabled -vault-allow-unauthenticated=false > /dev/null 2>&1 &
+    echo "Nomad agent not running, starting it.."
+    sudo nomad agent -dev -bind 0.0.0.0 -acl-enabled -vault-address=$VAULT_ADDR -vault-token $VAULT_TEST_TOKEN -vault-enabled -vault-allow-unauthenticated=false > /dev/null 2>&1 &
     export NOMAD_PID=$!
     echo $NOMAD_PID > /tmp/nomad-test.pid
 
@@ -29,5 +36,6 @@ if [ ! -e /tmp/nomad-test.pid ]; then
     http --ignore-stdin POST http://localhost:4646/v1/acl/bootstrap | jq -r '.SecretID' > /tmp/nomad-test.token
     echo export NOMAD_TOKEN=$(cat /tmp/nomad-test.token)
 elif [ -e /tmp/nomad-test.token ]; then 
+  echo "?Nomad agent already running"
   echo export NOMAD_TOKEN=$(cat /tmp/nomad-test.token)
 fi
