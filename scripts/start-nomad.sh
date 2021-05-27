dbc7d572cca0186fdc9490663c3fb916ac2ce133
Michele <mdeggies@gmail.com>
2021-05-26 17:52:39 -0700

    test

--- scripts/start-nomad.sh
+++ scripts/start-nomad.sh
@@ -32,9 +32,11 @@ if [ ! -e /tmp/nomad-test.pid ]; then
 
     http --ignore-stdin POST http://localhost:4646/v1/acl/bootstrap | jq -r '.SecretID' > /tmp/nomad-test.token
     NOMAD_TOKEN=$(cat /tmp/nomad-test.token)
+    echo "Nomad token: $NOMAD_TOKEN"
     echo "NOMAD_TOKEN=$(echo $NOMAD_TOKEN)" >> $GITHUB_ENV
 elif [ -e /tmp/nomad-test.token ]; then 
   echo "Nomad agent already running"
   NOMAD_TOKEN=$(cat /tmp/nomad-test.token)
+  echo "Nomad token: $NOMAD_TOKEN"
   echo "NOMAD_TOKEN=$(echo $NOMAD_TOKEN)" >> $GITHUB_ENV
 fi
