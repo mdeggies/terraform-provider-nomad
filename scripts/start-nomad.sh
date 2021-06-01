280590807e2d7653bd895f433b956f5a36f9d521
Michele <mdeggies@gmail.com>
2021-05-31 20:40:24 -0700

    See if this works..

--- scripts/start-nomad.sh
+++ scripts/start-nomad.sh
@@ -18,7 +18,7 @@ if [ ! -e /tmp/consul-test.pid ]; then
 fi
 
 if [ ! -e /tmp/nomad-test.pid ]; then
-    sudo nomad agent -dev -acl-enabled -vault-address=$VAULT_ADDR -vault-token $VAULT_TEST_TOKEN -vault-enabled -vault-allow-unauthenticated=false
+    sudo nomad agent -dev -acl-enabled -vault-address=$VAULT_ADDR -vault-token $VAULT_TEST_TOKEN -vault-enabled -vault-allow-unauthenticated=false > /dev/null 2>&1 &
     NOMAD_PID=$!
     echo $NOMAD_PID > /tmp/nomad-test.pid
 
